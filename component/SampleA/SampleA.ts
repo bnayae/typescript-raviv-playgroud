53a0522162e117635c944c3156dce45e5db103c5
bnaya eshet <bnayanet@hotmail.com>
2021-04-13 15:27:06 +0300

    [UPD] sample A

--- component/SampleA/SampleA.ts
+++ component/SampleA/SampleA.ts
@@ -2,8 +2,46 @@ import styled from 'styled-components';
 import { SampleARaw } from './SampleARaw';
 
 export const SampleA = styled(SampleARaw)`
+  display: grid;
+  grid-template-areas:
+    'title  title title'
+    'button input  .'
+    'list   list  list';
+  /* justify-content: center;
+  justify-items: center; */
+
+  grid-template-columns: 1fr 1fr 2fr;
+  grid-template-rows: 10rem auto;
+  grid-row-gap: 2rem;
+
   background: red;
   &.primary {
     background: pink;
+  }
+
+  .title {
+    grid-area: title;
+    font-size: 5rem;
+    justify-self: center;
+  }
+
+  .candidate {
+    grid-area: input;
+  }
+
+  .btn {
+    grid-area: button;
+  }
+
+  .items {
+    grid-area: list;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content: center;
+    justify-items: center;
+    grid-row-gap: 1rem;
+    .item {
+      background: lightblue;
+    }
   }
 `;
--- component/SampleA/SampleARaw.tsx
+++ component/SampleA/SampleARaw.tsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ISampleAProps } from './ISampleAProps';
 
 export const SampleARaw = ({ className, text, color }: ISampleAProps) => {
-  return <div className={`${className} ${color}`}>{text}</div>;
+  const [candidate, setCandidate] = useState<string>('');
+  const [list, setList] = useState<string[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCandidate(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setList((prv) => {
+      var set = new Set<string>(prv);
+      if (set.has(candidate)) return prv;
+      // if (prv.includes(candidate)) return prv;
+      return [...prv, candidate];
+    });
+    setCandidate('');
+  };
+
+  return (
+    <div className={`${className} ${color}`}>
+      <div className="title">{text}</div>
+      <input
+        className="candidate"
+        type="text"
+        value={candidate}
+        onChange={handleChange}
+      />
+      <button className="btn" onClick={handleAdd}>
+        Add
+      </button>
+      <div className="items">
+        {list.map((m) => (
+          <div className="item">{m}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
